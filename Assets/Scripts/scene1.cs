77093ef2ebb21a66ddb4f6e6543d57a6c7b237f3
Justinasss <96245670+Justinasss@users.noreply.github.com>
2025-06-14 23:00:11 +0300

    final

---
+++ Assets/Scripts/scene1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene1 : MonoBehaviour
+{
+    public string nextScene;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+
+        print(other.gameObject.name);
+    }
+}
